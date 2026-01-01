2a00bdf1a64fbc60550589b6500d10583c1e103d
Swapnil Kuthe <swapnilkuthe20@gmail.com>
2026-01-01 20:21:52 +0530

    Check if a Number is Even or Odd-- done

---
+++ isOddEven.js
@@ -0,0 +1,14 @@
+//  Write a js function that Check if a Number is Even or Odd ::
+
+
+const isOddEven = (num) => {
+
+    if (num % 2 == 0) {
+        console.log(`${num} is a Even`);
+    } else {
+        console.log(`${num} is a Odd`);
+    }
+
+}
+
+isOddEven(221334)
