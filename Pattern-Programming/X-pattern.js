076826e4f94e0939094e4e7382ac0c0e3b1003a2
Swapnil Kuthe <swapnilkuthe20@gmail.com>
2026-05-28 12:05:51 +0530

    Diaonal X-Pattrn sceanrio done successfully

---
+++ Pattern-Programming/X-pattern.js
@@ -0,0 +1,27 @@
+// *   *
+//  * *
+//   *
+//  * *
+// *   *
+
+
+let n = 5;
+let str = "";
+
+for (let i = 1; i <= n; i++) {
+
+    for (let j = 1; j <= n; j++) {
+
+        if (i == j || (i + j == 6)) {
+            str += "*"
+        } else {
+            str += " "
+        }
+    }
+
+    str += "\n";
+}
+
+
+console.log(str);
+
