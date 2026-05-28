dc0fbc7fe3bd245d32db9aa69a83f5c98a4293a8
Swapnil Kuthe <swapnilkuthe20@gmail.com>
2026-05-28 11:33:37 +0530

    hallow diamond sceanrio optimised code done successfully

--- Pattern-Programming/hallowDiamond.js
+++ Pattern-Programming/hallowDiamond.js
@@ -10,7 +10,7 @@
 let n = 4;
 let m = 3;
 let col = 7;
-let str = ""
+let str = "";
 
 // //  Upper Half : 
 
@@ -23,7 +23,7 @@ for (let i = 1; i <= n; i++) {
         if ((j == n - i + 1) || (j == n + i - 1)) {
             str += "*";
         } else {
-            str += " "
+            str += " ";
         }
     }
 
@@ -47,11 +47,58 @@ for (let k = 1; k <= m; k++) {
         }
     }
 
-    str += "\n"
+    str += "\n";
 
 }
 
-
 console.log(str);
 
+// --------------------------------------------------
 
+//  Optimised Version:
+//         1. insted m var we use m = n-1
+//         2. (l == 2 * m + 1 - k) insted of this we know col so, (l==col-1) 
+
+
+let row = 4;
+let cols = 7;
+let str2 = "";
+
+// // Upper half :: 
+
+for (let i = 1; i <= row; i++) {
+
+    for (let j = 1; j <= cols; j++) {
+
+        if ((j == row - i + 1) || (j == row + i - 1)) {
+            str2 += "*";
+        } else {
+            str2 += " ";
+        }
+    }
+
+    str2 += "\n";
+}
+
+// // Lower Half ::
+
+
+for (let a = 1; a < row; a++) {
+    // console.log(a, "...a");
+
+    for (let b = 1; b <= cols; b++) {
+
+        if ((b == a + 1) || (b == cols - a)) {
+            str2 += "*";
+        } else {
+            str2 += " ";
+        }
+    }
+
+    str2 += "\n"
+}
+
+
+console.log(str2);
+
+// -------------------------------------------
